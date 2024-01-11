ceaf137f072cda8397de58a477e778ee72e83947
AndresLince <lynxdarmatron@gmail.com>
2024-01-11 17:52:38 -0500

    Add app user class

---
+++ lib/app_user.dart
@@ -0,0 +1,6 @@
+/// Simple class representing the user UID and email.
+class AppUser {
+  const AppUser({required this.uid});
+  final String uid;
+// TODO: Add other fields as needed (email, displayName etc.)
+}
