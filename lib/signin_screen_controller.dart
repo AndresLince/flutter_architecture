0aa9607dae08adb6f07923c8793ad54cfac0d8af
AndresLince <lynxdarmatron@gmail.com>
2024-01-12 06:49:48 -0500

    Add signin screen controller

---
+++ lib/signin_screen_controller.dart
@@ -0,0 +1,10 @@
+import 'dart:async';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class SignInScreenController extends AsyncNotifier<void> {
+  @override
+  FutureOr<void> build() {
+    // no-op
+  }
+}
