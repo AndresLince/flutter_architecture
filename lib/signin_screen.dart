86895257f190c9cca243effea70de0a7c3b696eb
AndresLince <lynxdarmatron@gmail.com>
2024-01-12 06:36:19 -0500

    Add authRepositoryProvider on signin screen

--- lib/signin_screen.dart
+++ lib/signin_screen.dart
@@ -2,6 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import 'auth_repository.dart';
+import 'fake_auth_repository.dart';
+
+final authRepositoryProvider = Provider<AuthRepository>((ref) {
+  // return a concrete implementation of AuthRepository
+  return FakeAuthRepository();
+});
+
 class SignInScreen extends ConsumerWidget {
   const SignInScreen({Key? key}) : super(key: key);
 
@@ -14,7 +22,7 @@ class SignInScreen extends ConsumerWidget {
       body: Center(
         child: ElevatedButton(
           child: Text('Sign in anonymously'),
-          onPressed: () { /* TODO: Implement */ },
+          onPressed: () => ref.read(authRepositoryProvider).signInAnonymously(),
         ),
       ),
     );
