7c164a25382a07658a9cfda27af251180a631a28
AndresLince <lynxdarmatron@gmail.com>
2024-01-12 06:44:10 -0500

    Convert signing screen to ConsumerStatefulWidget

--- lib/signin_screen.dart
+++ lib/signin_screen.dart
@@ -10,11 +10,43 @@ final authRepositoryProvider = Provider<AuthRepository>((ref) {
   return FakeAuthRepository();
 });
 
-class SignInScreen extends ConsumerWidget {
+class SignInScreen extends ConsumerStatefulWidget {
   const SignInScreen({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<SignInScreen> createState() => _SignInScreenState();
+}
+
+class _SignInScreenState extends ConsumerState<SignInScreen> {
+  // keep track of the loading state
+  bool isLoading = false;
+
+  // call this from the `onPressed` callback
+  Future<void> _signInAnonymously() async {
+    try {
+      // update the state
+      setState(() => isLoading = true);
+      // sign in using the repository
+      await ref
+          .read(authRepositoryProvider)
+          .signInAnonymously();
+    } catch (e) {
+      // show a snackbar if something went wrong
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text(e.toString())),
+      );
+    } finally {
+      // check if we're still on this screen (widget is mounted)
+      if (mounted) {
+        // reset the loading state
+        setState(() => isLoading = false);
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
     return Scaffold(
       appBar: AppBar(
         title: const Text('Sign In'),
@@ -22,7 +54,7 @@ class SignInScreen extends ConsumerWidget {
       body: Center(
         child: ElevatedButton(
           child: Text('Sign in anonymously'),
-          onPressed: () => ref.read(authRepositoryProvider).signInAnonymously(),
+          onPressed: () => _signInAnonymously(),
         ),
       ),
     );
