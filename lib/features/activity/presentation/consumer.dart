2430591a2e0613907656ab5fba3f8a4a08fd2cbd
AndresLince <lynxdarmatron@gmail.com>
2024-01-13 14:50:52 -0500

    Add Home widget

---
+++ lib/features/activity/presentation/consumer.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../data/provider.dart';
+import '../domain/activity.dart';
+
+
+
+/// The homepage of our application
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer(
+      builder: (context, ref, child) {
+        // Read the activityProvider. This will start the network request
+        // if it wasn't already started.
+        // By using ref.watch, this widget will rebuild whenever the
+        // the activityProvider updates. This can happen when:
+        // - The response goes from "loading" to "data/error"
+        // - The request was refreshed
+        // - The result was modified locally (such as when performing side-effects)
+        // ...
+        final AsyncValue<Activity> activity = ref.watch(activityProvider);
+
+        return Center(
+          /// Since network-requests are asynchronous and can fail, we need to
+          /// handle both error and loading states. We can use pattern matching for this.
+          /// We could alternatively use `if (activity.isLoading) { ... } else if (...)`
+          child: switch (activity) {
+            AsyncData(:final value) => Text('Activity: ${value.activity}', style: TextStyle(
+              fontSize: 12,
+            ),),
+            AsyncError() => const Text('Oops, something unexpected happened'),
+            _ => const CircularProgressIndicator(),
+          },
+        );
+      },
+    );
+  }
+}
