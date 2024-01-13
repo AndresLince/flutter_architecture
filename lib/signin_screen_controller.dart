7a7f42cc704689aa17754f4305e1a03ac0c6ee6a
AndresLince <lynxdarmatron@gmail.com>
2024-01-13 09:36:50 -0500

    Add signInAnonymously function to signin screen controller

--- lib/signin_screen_controller.dart
+++ lib/signin_screen_controller.dart
@@ -1,12 +1,26 @@
 import 'dart:async';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
+import 'auth_repository.dart';
+import 'fake_auth_repository.dart';
+
 part 'signin_screen_controller.g.dart';
+
+final authRepositoryProvider = Provider<AuthRepository>((ref) {
+  // return a concrete implementation of AuthRepository
+  return FakeAuthRepository();
+});
 
 @riverpod
 class SignInScreenController extends _$SignInScreenController {
   @override
   FutureOr<void> build() {
     // no-op
+  }
+
+  Future<void> signInAnonymously() async {
+    final authRepository = ref.read(authRepositoryProvider);
+    state = const AsyncLoading();
+    state = await AsyncValue.guard(authRepository.signInAnonymously);
   }
 }
