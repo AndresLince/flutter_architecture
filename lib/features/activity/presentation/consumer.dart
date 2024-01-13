791c54e445ef3c61ee70d9c982288a28ac22a500
AndresLince <lynxdarmatron@gmail.com>
2024-01-13 15:28:53 -0500

    Use ConsumerWidget on home widget

--- lib/features/activity/presentation/consumer.dart
+++ lib/features/activity/presentation/consumer.dart
@@ -4,38 +4,28 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../data/provider.dart';
 import '../domain/activity.dart';
 
-
-
-/// The homepage of our application
-class Home extends StatelessWidget {
+/// We subclassed "ConsumerWidget" instead of "StatelessWidget".
+/// This is equivalent to making a "StatelessWidget" and retuning "Consumer".
+class Home extends ConsumerWidget {
   const Home({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Consumer(
-      builder: (context, ref, child) {
-        // Read the activityProvider. This will start the network request
-        // if it wasn't already started.
-        // By using ref.watch, this widget will rebuild whenever the
-        // the activityProvider updates. This can happen when:
-        // - The response goes from "loading" to "data/error"
-        // - The request was refreshed
-        // - The result was modified locally (such as when performing side-effects)
-        // ...
-        final AsyncValue<Activity> activity = ref.watch(activityProvider);
+  // Notice how "build" now receives an extra parameter: "ref"
+  Widget build(BuildContext context, WidgetRef ref) {
+    // We can use "ref.watch" inside our widget like we did using "Consumer"
+    final AsyncValue<Activity> activity = ref.watch(activityProvider);
 
-        return Center(
-          /// Since network-requests are asynchronous and can fail, we need to
-          /// handle both error and loading states. We can use pattern matching for this.
-          /// We could alternatively use `if (activity.isLoading) { ... } else if (...)`
-          child: switch (activity) {
-            AsyncData(:final value) => Text('Activity: ${value.activity}', style: TextStyle(
-              fontSize: 12,
-            ),),
-            AsyncError() => const Text('Oops, something unexpected happened'),
-            _ => const CircularProgressIndicator(),
-          },
-        );
+    // The rendering logic stays the same
+    return Center(
+      /// Since network-requests are asynchronous and can fail, we need to
+      /// handle both error and loading states. We can use pattern matching for this.
+      /// We could alternatively use `if (activity.isLoading) { ... } else if (...)`
+      child: switch (activity) {
+        AsyncData(:final value) => Text('Activity: ${value.activity}', style: TextStyle(
+          fontSize: 12,
+        ),),
+        AsyncError() => const Text('Oops, something unexpected happened'),
+        _ => const CircularProgressIndicator(),
       },
     );
   }
